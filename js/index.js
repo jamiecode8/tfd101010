fceb811e2d65162e798c386f0b686d8d67db6985
jamiecode8 <jamiechiu2020@gmail.com>
2021-03-25 15:04:55 +0800

    add

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// loading動畫
+//loading動畫
 // var textWrapper = document.querySelector('.ml12');
 // textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
@@ -35,13 +35,8 @@
 
 
 // 入口區塊動畫
-  // TweenMax.fromTo('.img_up', 1.5, { x: 10, y:10 }, { x: 300, y:200 });
-
-  // TweenMax.from(".img_up", 5 ,{ 
-  //   x:200 ,
-  //   repeat: -1,
-  // });
-
-  TweenMax
-  .To('.p1', 2, { opacity: 0 }, { opacity: 1 })
-  .to('.p2', 2, { opacity: 0 }, { opacity: 1 })
+var tl1 = new TimelineMax();
+  tl1.fromTo('.img_up', 1, { x: -90, y:90, opacity: 0 }, { x: 0, y:30, opacity: 1 
+  }).fromTo('.img_dw', 1, { x: 90, y:-90, opacity: 0 }, { x: 30, y:0, opacity: 1 
+  }).fromTo('.p1', 1, { opacity: 0 , delay: -5}, { opacity: 1 
+  }).fromTo('.p2', 1, { opacity: 0  , delay: -.5}, { opacity: 1 });
